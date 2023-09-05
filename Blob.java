2f135dd74160a90933598c70998d89b82444f73b
skiergr <80352508+skiergr@users.noreply.github.com>
2023-09-05 14:39:00 -0700

    Remake Blob and Add File Contents Method

--- Blob.java
+++ Blob.java
@@ -1,8 +1,17 @@
+import java.io.*;
+
 public class Blob {
-    String fileName;
+    public Blob(String fileName) {
 
-    public Blob(String fileInput) {
-        fileName = fileInput;
+    }
 
+    public String getFileContents(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        StringBuilder contents = new StringBuilder();
+        while (reader.ready()) {
+            contents.append((char) reader.read());
+        }
+        reader.close();
+        return contents.toString();
     }
 }
