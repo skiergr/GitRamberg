57be33eb22b3600318dac8b29455454e3a6d2955
Natalie Lim <natalielim1906@gmail.com>
2023-09-19 09:59:46 -0700

    fixed tree, still need to fix junit tester

--- Tree.java
+++ Tree.java
@@ -1,21 +1,67 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Formatter;
 
 public class Tree {
     private ArrayList<String> entries;
+    private String currentFileName;
 
     public Tree() throws IOException {
         entries = new ArrayList<String>();
-        File file = new File("test/objects/tree");
+        String sha = convertToSha1("");
+        File file = new File("test/objects/" + sha);
+        this.currentFileName = "test/objects/" + sha;
         if (!file.exists()) {
             file.createNewFile();
         }
     }
+
+    public static String convertToSha1(String fileContents) {
+        String sha1 = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(fileContents.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    // Used for sha1
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
     
     public void add (String line ) throws IOException {
         entries.add(line);
+        String contents = "";
+        BufferedReader br =  new BufferedReader(new FileReader(currentFileName));
+        while (br.ready()) {
+            contents += (char) br.read();
+        }
+        String sha = convertToSha1(contents);
+        File file = new File("test/objects/" + sha);
+        File file2 = new File (currentFileName);
+        file2.delete();
+        file.createNewFile();
+        currentFileName = "test/objects/" + sha;
         printHash();
     }
 
@@ -25,11 +71,22 @@ public class Tree {
                 entries.remove(i);
             }
         }
+        String contents = "";
+        BufferedReader br =  new BufferedReader(new FileReader(currentFileName));
+        while (br.ready()) {
+            contents += (char) br.read();
+        }
+        String sha = convertToSha1(contents);
+        File file = new File("test/objects/" + sha);
+        File file2 = new File (currentFileName);
+        file2.delete();
+        file.createNewFile();
+        currentFileName = "test/objects/" + sha;
         printHash();
     }
     
     public void printHash () throws IOException {
-        File file = new File("./test/objects/tree");
+        File file = new File(currentFileName);
         FileWriter writer = new FileWriter(file);
         for (int i = 0; i < entries.size(); i++) {
             writer.write(entries.get(i) + "\n");
